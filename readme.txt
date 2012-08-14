916ce1c2f8b551bed6f1ec8c37d599a8a17f88e3
Jason Lau <Jason Lau@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-14 03:20:57 +0000

    Made a slight change to the new json shorttag attribute.
    git-svn-id: https://plugins.svn.wordpress.org/user-meta-manager/trunk@585173 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Contributors: Jason Lau
 Donate link: http://jasonlau.biz
 Tags: user, users, meta, usermeta, wp_usermeta, data, table, database, edit, add, update, delete, save, saved, list, manage, manager, management, customize, custom, create, activate, register, registers, short, tag, shorttag, member, members, admin, administrate, administrator, administrative, tool, plugin, module, addon, jason, lau, jasonlau, jasonlau.biz
 Requires at least: 3.3.1 
-Stable tag: 1.5.3
+Stable tag: 1.5.4
 Tested up to: 3.4.1
 
 == Description ==
@@ -77,6 +77,9 @@ Minor code changes.
 = 1.5.3 =
 Added a new shorttag attribute, which allows you to restrict content based upon multiple meta keys and values.
 
+= 1.5.4 =
+Made a slight change to the new json shorttag attribute.
+
 == Upgrade Notice ==
 
 = 1.0 =
@@ -108,3 +111,6 @@ Minor code changes.
 
 = 1.5.3 =
 Added a new shorttag attribute, which allows you to restrict content based upon multiple meta keys and values.
+
+= 1.5.4 =
+Made a slight change to the new json shorttag attribute.
--- user-meta-manager.php
+++ user-meta-manager.php
@@ -4,7 +4,7 @@
  * Plugin Name: User Meta Manager
  * Plugin URI: http://websitedev.biz
  * Description: Add, edit, or delete user meta data with this handy plugin. Easily restrict access or insert user meta data into posts or pages.
- * Version: 1.5.3
+ * Version: 1.5.4
  * Author: Jason Lau
  * Author URI: http://websitedev.biz
  * Text Domain: user-meta-manager
@@ -31,7 +31,7 @@
     exit('Please don\'t access this file directly.');
 }
 
-define('UMM_VERSION', '1.5.3');
+define('UMM_VERSION', '1.5.4');
 define("UMM_PATH", plugin_dir_path(__FILE__) . '/');
 
 if(!class_exists('WP_List_Table')):
@@ -485,12 +485,13 @@ function umm_useraccess_shorttag($atts, $content) {
     $message = $atts['message'];
     
     if($atts['json']):
+    $access = false;
       $json = json_decode($atts['json']);
       foreach($json as $k => $v):
         if($k && $v):
           $meta_value = get_user_meta($current_user->ID, $k, true);
-          if($meta_value != trim($v)):        
-            $access = false;
+          if($meta_value == trim($v)):        
+            $access = true;
           endif;  
         endif;
     endforeach; 
