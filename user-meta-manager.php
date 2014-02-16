3667c259f108fa09791af8119b564582af44f7bb
Jason Lau <Jason Lau@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-16 09:03:20 +0000

    Fixed a bug in checkbox group validation.
    git-svn-id: https://plugins.svn.wordpress.org/user-meta-manager/trunk@858775 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- user-meta-manager.php
+++ user-meta-manager.php
@@ -4,7 +4,7 @@
  * Plugin Name: User Meta Manager
  * Plugin URI: https://github.com/jasonlau/Wordpress-User-Meta-Manager
  * Description: Add, edit, or delete user meta data with this handy plugin. Easily restrict access or insert user meta data into posts or pages and more. <strong>Get the Pro extension <a href="http://jasonlau.biz/home/membership-options#umm-pro">here</a>.</strong>
- * Version: 3.1.9
+ * Version: 3.2.0
  * Author: Jason Lau
  * Author URI: http://jasonlau.biz
  * Text Domain: user-meta-manager
@@ -31,7 +31,7 @@
     exit('Please don\'t access this file directly.');
 }
 
-define('UMM_VERSION', '3.1.9');
+define('UMM_VERSION', '3.2.0');
 define("UMM_PATH", plugin_dir_path(__FILE__) . '/');
 define("UMM_SLUG", "user-meta-manager");
 define("UMM_AJAX", "admin-ajax.php?action=umm_switch_action&amp;umm_sub_action=");
@@ -2556,7 +2556,8 @@ function umm_validate_profile_fields($errors, $update, $user) {
 	$saved_profile_fields = (!umm_get_option('profile_fields')) ? array() : umm_get_option('profile_fields');
     if(isset($_REQUEST['umm_nonce']) && wp_verify_nonce($_REQUEST['umm_nonce'], 'umm_wp_nonce')):
        foreach($saved_profile_fields as $field_name => $field_settings):      
-         $field_value = (isset($_REQUEST[$field_name])) ? htmlspecialchars(trim($_REQUEST[$field_name])) : '';
+         $field_value = (isset($_REQUEST[$field_name])) ? $_REQUEST[$field_name] : '';
+         $field_value = (!is_array($field_value)) ? htmlspecialchars(trim($field_value)) : $field_value;
          if(!$field_settings['allow_tags']) $field_value = wp_strip_all_tags($field_value);
          if($field_settings['unique'] == 'yes' && umm_is_duplicate($field_name, $field_value, $user->ID)):
             $errors->add( $field_name, __('<strong>ERROR</strong>: <em>' . $field_settings['label'] . '</em> is already taken by another user. Please use a different selection.', UMM_SLUG) );
